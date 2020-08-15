5ffc48a3338ccfc013d4a0682585bd0f60974ac2
cho1490 <chosanghyun99@gmail.com>
2020-08-16 01:51:04 +0900

    Add Main xml

--- app/src/main/java/com/instagram_clone/LoginActivity.kt
+++ app/src/main/java/com/instagram_clone/LoginActivity.kt
@@ -4,19 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
-    var auth: FirebaseAuth? = null;
+    var auth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        auth = FirebaseAuth.getInstance();
+        auth = FirebaseAuth.getInstance()
 
         btn_email_login.setOnClickListener {
             signInAndSignUp()
@@ -29,7 +35,7 @@ class LoginActivity : AppCompatActivity() {
                 if(task.isSuccessful){
                     moveMainPage(task.result?.user)
                 }else if(task.exception?.message.isNullOrEmpty()){
-                    Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show();
+                    Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
                 }else{
                     signInEmail()
                 }
@@ -37,20 +43,20 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun signInEmail(){
-        auth?.signInWithEmailAndPassword(et_email.text.toString(), et_password.text.toString())?.addOnCompleteListener{
-                task->
+        auth?.signInWithEmailAndPassword(et_email.text.toString(), et_password.text.toString())
+            ?.addOnCompleteListener{
+                    task->
             if(task.isSuccessful){
                 moveMainPage(task.result?.user)
             }else{
-                Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show();
+                Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
             }
         }
     }
 
     private fun moveMainPage(user:FirebaseUser?){
         if(user != null){
-            startActivity(Intent(this,MainActivity::class.java));
+            startActivity(Intent(this,MainActivity::class.java))
         }
     }
-
 }
