33d1cc62fc7b0629a8ed6f2042c54275d2b6dec2
cho1490 <chosanghyun99@gmail.com>
2020-08-19 04:13:12 +0900

    Finished favoriteEvent

--- app/src/main/java/com/instagram_clone/navigation/DetailViewFragment.kt
+++ app/src/main/java/com/instagram_clone/navigation/DetailViewFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.instagram_clone.R
 import com.instagram_clone.navigation.model.ContentDTO
@@ -17,6 +18,7 @@ import kotlinx.android.synthetic.main.item_detail.view.*
 class DetailViewFragment : Fragment(){
 
     var firestore : FirebaseFirestore? = null
+    var uid : String? = null;
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,6 +27,7 @@ class DetailViewFragment : Fragment(){
     ): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_detail , container, false)
         firestore = FirebaseFirestore.getInstance()
+        uid = FirebaseAuth.getInstance().currentUser?.uid
 
         view.detailviewfragment_recyclerview.adapter = DetailViewRecyclerViewAdapter()
         view.detailviewfragment_recyclerview.layoutManager = LinearLayoutManager(activity)
@@ -76,6 +79,39 @@ class DetailViewFragment : Fragment(){
             viewHolder.detailviewitem_favoritecount_textview.text = "좋아요  " + contentDTOs!![position].favoriteCount
             // profile image
             Glide.with(holder.itemView.context).load(contentDTOs!![position].imageUrl).into(viewHolder.detailviewitem_profile_image )
+            // button click event
+            viewHolder.detailviewitem_favorite_imageivew.setOnClickListener {
+                favoriteEvent(position)
+            }
+
+            if(contentDTOs!![position].favorites.containsKey(uid)){
+                // like status
+                viewHolder.detailviewitem_favorite_imageivew.setImageResource(R.drawable.ic_favorite)
+            }else{
+                // unlike status
+                viewHolder.detailviewitem_favorite_imageivew.setImageResource(R.drawable.ic_favorite_border )
+            }
+
         }
+
+        fun favoriteEvent(position : Int){
+            var tsDoc = firestore?.collection("images")?.document(contentUidLists[position])
+            firestore?.runTransaction { transaction ->
+                var uid = FirebaseAuth.getInstance().currentUser?.uid
+                var contentDTO = transaction.get(tsDoc!!).toObject(ContentDTO::class.java)
+
+                if(contentDTO!!.favorites.containsKey(uid)){
+                    // button clicked
+                    contentDTO?.favoriteCount = contentDTO.favoriteCount - 1
+                    contentDTO?.favorites.remove(uid)
+                }else{
+                    // button not clicked
+                    contentDTO?.favoriteCount = contentDTO.favoriteCount + 1
+                    contentDTO?.favorites[uid!!] = true
+                }
+                transaction.set(tsDoc, contentDTO)
+            }
+        }
+
     }
 }
