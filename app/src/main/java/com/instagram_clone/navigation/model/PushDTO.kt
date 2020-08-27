401e7465fb291415666d23129758aebcbf84c265
cho1490 <chosanghyun99@gmail.com>
2020-08-27 22:51:23 +0900

    Add PushDTO

---
+++ app/src/main/java/com/instagram_clone/navigation/model/PushDTO.kt
@@ -0,0 +1,9 @@
+package com.instagram_clone.navigation.model
+
+
+data class PushDTO(var to : String? = null,
+                   var notification : Notification = Notification()){
+
+    data class Notification(var body : String? = null,
+                            var title : String? = null)
+}
